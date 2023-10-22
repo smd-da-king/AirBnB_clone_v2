797104d2f7e26773d921f5c996419e836bc9e3f6
smd-da-king <toyin_ng@yahoo.com>
2023-10-22 00:44:20 -0700

    a

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Hello HBNB from Flask."""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Hello Home."""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', port=5000)
